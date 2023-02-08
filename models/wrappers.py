c4c774a7fce0ce8c70e998dee73850dcf6750f22
formanite-dev <82734646+formanite-dev@users.noreply.github.com>
2023-02-08 05:34:36 +0500

    Update wrappers.py

--- models/wrappers.py
+++ models/wrappers.py
@@ -146,8 +146,8 @@ class StyleGAN2(BaseModel):
             'places': 'https://drive.google.com/uc?export=download&id=1X8-wIH3aYKjgDZt4KMOtQzN1m4AlCVhm',
             'bedrooms': 'https://drive.google.com/uc?export=download&id=1nZTW7mjazs-qPhkmbsOLLA_6qws-eNQu',
             'kitchen': 'https://drive.google.com/uc?export=download&id=15dCpnZ1YLAnETAPB0FGmXwdBclbwMEkZ',
-            'lookbook': 'https://drive.google.com/uc?export=download&id=1-F-RMkbHUv_S_k-_olh43mu5rDUMGYKe'
-            #'lookbook': 'https://drive.google.com/file/d/17g1Gi9VRjfegwF_o-K3UUp__SMGiwFYf/view?usp=sharing'
+            #'lookbook': 'https://drive.google.com/uc?export=download&id=1-F-RMkbHUv_S_k-_olh43mu5rDUMGYKe'
+            'lookbook': 'https://drive.google.com/file/d/17g1Gi9VRjfegwF_o-K3UUp__SMGiwFYf/view?usp=sharing'
         }
 
         url = checkpoints[self.outclass]
